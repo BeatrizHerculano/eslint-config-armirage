8177e191656e4fe826d5bac11652fc59b51aa3a5
Ray (Clinton) Mulligan <12667615+ClintMulligan@users.noreply.github.com>
2020-09-30 02:29:00 -0400

    Update index.js
    Format copyright block, removed excessive tabs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 /* eslint-env node */
 
-/*! version: 1.0.1
-		author: Clinton Mulligan
-		license: BSD 3-Clause License
-		Copyright (c) 2020, Clinton Mulligan. All rights reserved.
-		Full License at https://github.com/armirage/eslint-config-armirage/blob/master/LICENSE.md !*/
+/*!
+	version: 1.0.1
+	author: Clinton Mulligan
+	license: BSD 3-Clause License
+	Copyright (c) 2020, Clinton Mulligan. All rights reserved.
+	Full License at https://github.com/armirage/eslint-config-armirage/blob/master/LICENSE.md
+!*/
 
 module.exports = {
 	"extends": [
